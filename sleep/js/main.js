542112a6f7858e440509b3d6a0b4e57070a59020
eyulw <alsgk6989@gmail.com>
2023-05-22 15:19:35 +0900

    modify sleep

--- sleep/js/main.js
+++ sleep/js/main.js
@@ -7,4 +7,14 @@ gnbList.forEach((item,idx)=>{
 });
 header.addEventListener("mouseleave",()=>{
     header.classList.remove("on");
-})
+});
+
+new Swiper(".media .mask",{
+    // li크기 자체가 줄어듦. 한번에 4개 볼수 있는만큼.
+    // "auto"쓰면 크기 내가 정해줘야함
+    slidesPerView:"auto",
+    centeredSlides: true,
+    // 슬라이드 사이 여백
+    spaceBetween:20, 
+    loop: true,
+});
